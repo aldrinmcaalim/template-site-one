34ce99731519f987f2c9af5e2c30e26bd928ab54
aldrinmcaalim <aldrinmcaalim@gmail.com>
2023-02-28 07:23:05 -0500

    made planned components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
